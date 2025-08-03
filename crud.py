300215217e531d124a9bc0f9ce441f177a69682c
Jonthemonk <jonsnider23@gmail.com>
2025-08-03 16:44:22 +0000

    crud returns values

--- crud.py
+++ crud.py
@@ -8,7 +8,7 @@ from core.errors import (
     DatabaseError
 )
 
-def add_employee(db: Session, face: FaceEmbedding):
+def add_employee(db: Session, face: FaceEmbedding) -> Employee:
     if db.query(Employee).filter(Employee.employee_id == face.employee_id).first():
         raise EmployeeAlreadyExists()
 
@@ -16,11 +16,14 @@ def add_employee(db: Session, face: FaceEmbedding):
         emp = Employee(**face.dict())
         db.add(emp)
         db.commit()
+        db.refresh(emp)
+        return emp
     except Exception as e:
         raise DatabaseError(f"Error adding employee: {str(e)}")
 
 
-def update_employee(db: Session, face: FaceEmbedding):
+
+def update_employee(db: Session, face: FaceEmbedding) -> Employee:
     emp = db.query(Employee).filter(Employee.employee_id == face.employee_id).first()
     if not emp:
         raise EmployeeNotFound()
@@ -30,11 +33,14 @@ def update_employee(db: Session, face: FaceEmbedding):
         emp.embedding = face.embedding
         emp.role = face.role
         db.commit()
+        db.refresh(emp)
+        return emp
     except Exception as e:
         raise DatabaseError(f"Error updating employee: {str(e)}")
 
 
-def remove_employee_by_id(db: Session, employee_id: str):
+
+def remove_employee_by_id(db: Session, employee_id: str) -> Employee:
     emp = db.query(Employee).filter(Employee.employee_id == employee_id).first()
     if not emp:
         raise EmployeeNotFound()
@@ -42,8 +48,10 @@ def remove_employee_by_id(db: Session, employee_id: str):
     try:
         db.delete(emp)
         db.commit()
+        return emp
     except Exception as e:
         raise DatabaseError(f"Error removing employee: {str(e)}")
+
 
 
 def get_all_employees(db: Session) -> List[Employee]:

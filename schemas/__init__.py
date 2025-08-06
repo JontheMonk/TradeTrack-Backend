d104a0093598929d868033c5cf1002acc16e0e54
Jonthemonk <jonsnider23@gmail.com>
2025-08-06 18:20:12 +0000

    added a dot

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,7 +1,7 @@
 from .face_record import FaceRecord
 from .verify_face_request import VerifyFaceRequest
 from .success_response import SuccessResponse
-from employee_search_result import EmployeeSearchResult
+from .employee_search_result import EmployeeSearchResult
 
 
 __all__ = ["FaceRecord", "VerifyFaceRequest", "SuccessResponse", "EmployeeSearchResult"]

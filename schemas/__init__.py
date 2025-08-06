8426bfea4ff1a020b6eb0cc4ced8b10db6d09e12
Jonthemonk <jonsnider23@gmail.com>
2025-08-06 19:50:45 +0000

    added some folders

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,7 +1,7 @@
-from .face_record import FaceRecord
-from .verify_face_request import VerifyFaceRequest
-from .success_response import SuccessResponse
-from .employee_search_result import EmployeeSearchResult
+from .input.face_record import FaceRecord
+from .input.verify_face_request import VerifyFaceRequest
+from .output.success_response import SuccessResponse
+from .output.employee_search_result import EmployeeSearchResult
 
 
 __all__ = ["FaceRecord", "VerifyFaceRequest", "SuccessResponse", "EmployeeSearchResult"]
